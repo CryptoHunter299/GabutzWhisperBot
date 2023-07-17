69848b8c01b265dccb3bfe398d0d29e0c0090c52
CryptoHunter299 <96936412+CryptoHunter299@users.noreply.github.com>
2023-07-17 13:42:23 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,6 +17,7 @@ plugins = dict(
 # logging.basicConfig(level=logging.DEBUG)
 print('>>> BOT STARTED')
 Client(
+    "GabutzWhisperBot",
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"],
     bot_token = os.environ["BOT_TOKEN"],
